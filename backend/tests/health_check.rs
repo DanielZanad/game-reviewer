e1a9673daf5da4a6d39b32c16322bdb54b030a39
Daniel Zanad <daniel.zanad.dz@gmail.com>
2024-12-15 19:14:42 -0300

    add: health_check test

---
+++ backend/tests/health_check.rs
@@ -0,0 +1,38 @@
+use std::net::TcpListener;
+
+#[tokio::test]
+
+// the health check is exposed at /health_check;
+// the health check is behind a GET method;
+// the health check always returns a 200;
+// the health check’s response has no body.
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// Launch application in the background ~somehow~
+fn spawn_app() -> String{
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = backend::run(listener).expect("Failed to bind address");
+
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future
+    // but we have no use for it here, hence the non-binding let
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
